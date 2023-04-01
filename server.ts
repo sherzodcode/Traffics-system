348e3e5c3fe3f216ba1e4ba78f4a1bb7245fc027
sherzodcode <example@gmail.com>
2023-04-01 23:13:46 +0500

    Tailwind is worked

--- server.ts
+++ server.ts
@@ -1,12 +1,19 @@
 import express from "express"
 import cors from "cors"
+import {engine} from "express-handlebars"
+import {Request , Response} from  "express"
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+app.engine('hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
 
+app.get('/', (req: Request, res: Response) => {
+    res.render('news')
+})
 
 app.listen(8080, () => {
     console.log("Server is running");
