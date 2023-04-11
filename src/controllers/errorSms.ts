bb84d401c03e92daf11925605b6bbf15a3835b2f
sherzodcode <sherzodcode@gmail.com>
2023-04-11 12:44:48 +0500

    Code cleaning

--- src/controllers/errorSms.ts
+++ src/controllers/errorSms.ts
@@ -23,3 +23,4 @@ export default async(req: Request, res: Response) => {
         })
     }
 }
+
--- src/controllers/getPages.ts
+++ src/controllers/getPages.ts
@@ -7,16 +7,18 @@ export const AddSmsPage = (req: Request, res: Response) => {
 }
 
 export const SmsCodesPage = (req: Request, res: Response) => {
-   return res.render('sms-codes') 
+    return res.render('sms-codes') 
 }
 
-export const SuccesfullyPage = (req: Request, res: Response) => {
-    return res.render('succesfully')
-}
 
-export const ErrorsPage = (req: Request, res: Response) => {
-    return res.render('errors')
-}
+// export const SuccesfullyPage = (req: Request, res: Response) => {
+//     return res.render('succesfully')
+// }
+
+// export const ErrorsPage = (req: Request, res: Response) => {
+//     return res.render('errors')
+// }
+
 
 export const SendingTitlePage = (req: Request, res: Response) => {
     return res.render('send-title')
